9000cd291a552076e91200a8613c7c3ee389a051
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-16 12:42:39 -0800

    changes

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+/**
+ *main- a program that prints a function using put function
+ *Return: 0 (Sucess)
+ */
+int main(void)
+{
+put("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
