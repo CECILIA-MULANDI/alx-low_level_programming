af82f4465ae098b4196a362d0be5486f74f0bc4a
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-21 10:41:27 -0800

    changes

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_last_digit - print the last digit
+ * @num: the argument
+ * Return: return the last digit
+ */
+
+
+int print_last_digit(int num)
+{
+	int lastdigit = num % 10;
+
+	return (lastdigit);
+}
+
+
