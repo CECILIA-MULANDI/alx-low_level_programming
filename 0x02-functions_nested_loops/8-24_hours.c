907d1617f085479b09f3ec183bdab13cc3ff5b53
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-21 11:28:34 -0800

    changes

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * jack_bauer - prints eery minute of the day
+ * Return: nothing is returned
+ */
+void jack_bauer(void)
+{
+	int start = 0, startend = 23, secondstart = 0, end = 59;
+
+	while (start <= startend)
+	{
+		while (secondstart < = end)
+		{
+			_putchar(start < 9 ? 0 + '0' : start / 10 + '0');
+			_putchar(start < 9 ? start + '0' : start % 10 + '0');
+			_putchar(':');
+			_putchar(secondstart < 9 ? 0 + '0' : secondstart / 10 + '0');
+			_putchar(secondstart < 9 ? secondstart + '0' : secondstart % 10 + '0');
+			secondstart++;
+		}
+		secondstart = 0;
+		start++;
+	}
+}
