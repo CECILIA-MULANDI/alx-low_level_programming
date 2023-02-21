db6074162085d813fa16274f831eba211fa485a2
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-21 10:50:53 -0800

    changes

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,13 +2,19 @@
 /**
  * print_last_digit - print the last digit
  * @num: the argument
- * Return: return the last digit
+ * Return: return the last digi
  */
 
 
 int print_last_digit(int num)
 {
 	int lastdigit = num % 10;
+
+	if (lastdigit < 0)
+		lastdigit = lastdigit * -1;
+	_putchar(lastdigit + '0');
+
+
 
 	return (lastdigit);
 }
