aea0a3f54495a900174b33733b3ac4f20c04bc8e
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-21 11:43:46 -0800

    changes

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @a - the arguments passed
+ * @b - the argument passed
+ * Return: result
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
