e0263624375c5e863f013c038c95c08deb0b0400
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-23 18:59:07 -0800

    changes

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,7 +13,7 @@ void print_triangle(int size)
 		for (a = size; a > 0; a--)
 		{
 			for (b = 1; b <= size; b++)
-				if (b >= a
+				if (b >= a)
 					_putchar('#');
 				else
 					_putchar(' ');
