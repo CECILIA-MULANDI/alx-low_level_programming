144433b5d9586ab5ec3428abd263f42951bb331f
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-24 15:51:34 -0800

    changes

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * prime_factors- function to get prime numbers
+ * Description: gets the largest prime number
+ * @num: argument
+ */
+void prime_factors(long num)
+{
+	long divisor;
+
+	while (divisor != num)
+	{
+		if (num % divisor == 0)
+		{
+			num /= divisor;
+		}
+		else
+		{
+			divisor++;
+		}
+	}
+	printf("%ld\n", num);
+}
+/**
+ * main(void)-starting point
+ * Return: 0
+ */
+int main(void)
+{
+	prime_factors(612852475143);
+	return (0);
+}
+
