4a14951234e03a10bed5ba57cf500b259c967524
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-23 18:34:06 -0800

    changes

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_square - prints a square
+ * @size: argument
+ * Return: int size
+ */
+void print_square(int size)
+{
+	int counter = 0;
+
+	int i;
+
+	if (size <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			while (counter < size)
+			{
+				_putchar('#');
+				counter;
+			}
+			counter = 0;
+			_putchar('\n');
+		}
+	}
+}
