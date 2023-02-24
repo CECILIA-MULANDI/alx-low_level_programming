cc774f488defaa068b0a1c3f5853d65b00768a22
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-23 16:38:44 -0800

    changes

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * mult - multiplies two nos
+ * @a:argument
+ * @b:argument
+ * Return:
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a*b;
+
+	return (product);
+}
