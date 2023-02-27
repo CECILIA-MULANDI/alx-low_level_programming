0087253391cfcbc5f646fa4a2adb62b8c08a354e
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-27 11:24:03 -0800

    changes

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - iterates through each character
+ * @str:parameter
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int len;
+
+	len = strlen(str);
+
+	for (i = 0; i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
+
