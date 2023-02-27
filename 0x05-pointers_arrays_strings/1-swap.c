d5a71732c6ace813ba8986094aac950a1e0caaa6
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-27 07:17:57 -0800

    changes

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *swap_int - swap to integers
+ *@a:parameter
+ *@b:parameter
+ *Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+
