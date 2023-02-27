47aa0323bec16ebf144bf06bbc09129b0b611c1a
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-02-27 11:39:07 -0800

    changes

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts_half - split the str into half
+ * @str: parameter
+ * Returns: void
+*/
+void puts_half(char *str)
+{
+	int counter, j;
+
+	while (*str)
+	{
+		counter++;
+		str++;
+	}
+	for (j = 0; j < counter; j++)
+		str--;
+	j = (counter % 2 == 0) ? counter / 2 : (counter + 1) / 2;
+
+	for (; j < counter; j++)
+		_putchar(str[i]);
+	_putchar('\n');
+
