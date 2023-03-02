6d86519277e0a210c18d57523ec9760b9f5b2dd1
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-02 15:53:56 -0800

    changes

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -9,19 +9,22 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	int len1 = strlen(src);
-
-
 	int i;
 
-	for (i = 0; i <= len1; i++)
+	i = 0;
+	while (i < n && src[i] != '\0')
 	{
-		if (i < n)
-		{
-			dest[i] = src[i];
-		}
+		dest[i] = src[i];
+		i++;
 	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
 	return (dest);
 }
+
 
 
