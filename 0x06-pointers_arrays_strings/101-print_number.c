6c29cb6eb907f06ed63c7a433d329ac538dc8663
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-02 18:00:26 -0800

    changes

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - print number
+ * @n: argument
+ * Return: 0
+ */
+
+void print_number(int n)
+{
+	int no;
+
+	no = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		no = -n;
+	}
+
+	if (no / 10 != 0)
+	{
+		print_number(no / 10);
+	}
+	_putchar((no % 10) + '0');
+}
+
+
