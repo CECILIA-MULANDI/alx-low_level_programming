c38210e945007e5e574f99fbf3bc0fcadbd1a887
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-07 09:42:55 -0800

    changes

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strpbrk -searches the string
+ * @s:argument
+ * @accept:argument
+ * Return:pointer
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	while (*s != '\0')
+	{
+		char *a = accept;
+
+		while (*a != '\0')
+		{
+			if (*s == *a)
+			{
+				return (s);
+			}
+			a++;
+		}
+		s++;
+	}
+	return (NULL);
+}
