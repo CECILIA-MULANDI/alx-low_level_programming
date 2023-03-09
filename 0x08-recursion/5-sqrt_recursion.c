70586f69b0180753bafacddf4ce29bd8822495fb
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-08 16:53:40 -0800

    changes

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,64 +3,39 @@
  *_sqrt_recursion- function to get sqrt
  *@n:argument
  *Return: int type
+ *@i:argument
  */
-
-/**
- *@low:argument
- *@high:argument
- */
-int sqrt_helper(int n, int low, int high);
+int _sqrt_helper(int n, int i);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
 	{
 		return (-1);
 	}
-	if (n == 0 || n == 1)
+	else
 	{
-		return (n);
+		return (_sqrt_helper(n, 0));
 	}
-	return (sqrt_helper(n, 1, n));
 }
 /**
- * sqrt_helper - does binary search to hep find the midpoint
+*_sqrt_helper - function
  * @n:argument
- * @low:argument
- * @high:argument
- * @high: argument used
- *Return:int
+ *@i:argument
+ * Return: of type int
  */
-int sqrt_helper(int n, int low, int high)
+int _sqrt_helper(int n, int i)
 {
-	int mid;
-
-	int mid_squared;
-
-	mid = (low + high) / 2;
-
-
-	mid_squared = mid * mid;
-
-	if (low > high)
+	if (i * i > n)
 	{
 		return (-1);
 	}
-
-
-	if (mid_squared == n)
+	else if (i * i == n)
 	{
-		return (mid);
-	}
-	else if (mid_squared < n)
-	{
-		return (sqrt_helper(n, mid + 1, high));
+		return (i);
 	}
 	else
 	{
-		return (sqrt_helper(n, low, mid - 1));
+		return (_sqrt_helper(n, i + 1));
 	}
 }
-
-
-
 
