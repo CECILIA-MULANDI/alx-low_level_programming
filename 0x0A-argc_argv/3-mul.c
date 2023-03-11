51c0139c5a5b0f210e69c29b001b5889ce5fc4bd
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-10 17:30:52 -0800

    changes

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - function to multiply two numbers
+ * @argc:argument
+ * @argv:argument
+ * Return:0 on success
+ */
+int main(int argc, char *argv[])
+{
+	int len = argc - 1;
+
+	if (len != 2)
+	{
+		printf("Error");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	return (0);
+
+
+}
