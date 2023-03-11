791ccae0346f64c4bcc542aa92cc408044b33bfb
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-10 16:39:29 -0800

    changes

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ * @argc:argument
+ * @argv:argument
+ * Return:int
+ */
+int main(int argc, int *argv[])
+{
+	printf("%d\n", argc);
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+argc and argv in c,let us take a deep dive now
