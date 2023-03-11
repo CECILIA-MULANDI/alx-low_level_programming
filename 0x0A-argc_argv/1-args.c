fcdef42d103fbbdafdc847e8e35129b47ed7ba5f
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-10 17:04:02 -0800

    changes

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main function
+ *@argc:argument
+ *@argv:argument
+ *Return:int
+ */
+int main(int argc, char *argv[]__attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
