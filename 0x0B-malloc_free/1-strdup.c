28a4a9b36a54d84eb3d9a400bc6b7ef81758a2dc
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-15 09:26:12 -0700

    changes

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ *_strdup-function returns a pointer
+ *@str:argument to duplicate
+ *Return: a character (ptr)
+ */
+char *_strdup(char *str)
+{
+	int size, i;
+
+	char *ptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	for (size = 0; str[size] != '\0'; size++)
+		;
+	ptr = (chaar *)malloc((size + 1) * sizeof(char)));
+
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		ptr[i] = str[i];
+	}
+	ptr[i] = '\0';
+
+	return (ptr);
+}
+
+
