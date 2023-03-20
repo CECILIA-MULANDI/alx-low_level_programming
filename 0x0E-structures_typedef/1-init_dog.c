a914551b76db9d288c139e74012e18127d12848c
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-20 09:40:06 -0700

    changes

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 /**
  * init_dog -function
  * @d:argument pointer
@@ -9,8 +10,13 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == NULL)
+		return;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
 
