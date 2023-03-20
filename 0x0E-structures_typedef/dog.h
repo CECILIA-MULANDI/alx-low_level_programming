64c9debc5d3b2fdc3c3fed308c988e5965fbea0c
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-20 09:06:01 -0700

    changes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,9 +8,9 @@
  */
 struct dog
 {
-	char* name;
+	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 typedef struct dog my_dog;
 void init_dog(struct dog *d, char *name, float age, char *owner);
