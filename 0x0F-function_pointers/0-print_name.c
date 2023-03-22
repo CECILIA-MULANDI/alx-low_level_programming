9303393fc56c1cc8aa6da476d6da4bfe33dab346
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-22 04:19:03 -0700

    changes

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ *print_name- function that prints the name
+ *@name:argument pointer to name
+ *@f:pointer to funtion
+ *Return:void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (!f || !name)
+		return;
+	f(name);
+}
+
