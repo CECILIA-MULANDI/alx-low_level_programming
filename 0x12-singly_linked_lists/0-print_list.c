d00f2b4becf58b85700e6f30bbb60bc2548a064d
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-30 12:54:00 -0700

    changes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ *print_list - function to print all elements
+ *@h: pointer argument to the head of the list
+ *Return: size_t - the count
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] (nil)\n", 0);
+		else
+		{
+			printf("[%d] %s\n", h->len, h->str);
+		}
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
+
