4cd0dad903e8bad85647452735b553dec3657f01
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-03-30 19:15:43 -0700

    changes

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+
+/**
+
+ * main - starting point
+
+ *
+
+ * Return: 0 on success
+
+ */
+
+int main(void)
+{
+	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
+	
+	return (0);
+  
+}
