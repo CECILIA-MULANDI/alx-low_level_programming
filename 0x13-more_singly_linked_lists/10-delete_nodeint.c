e361e14a67584f86cfecdf0fbccd9ce506431b83
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-04-05 18:37:46 -0700

    changes

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,9 +1,9 @@
 #include "lists.h"
 /**
- *
- *
- *
- *
+ *delete_nodeint_at_index - function to delete
+ *@head:argument pointer
+ *@index:argument pointer
+ *Return:int type
  *
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
