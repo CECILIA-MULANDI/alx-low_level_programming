8135febfa52fefe5a4726d30547cfee5313d2a32
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-04-05 18:51:02 -0700

    changes

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *prevNode = NULL;
 
-	listint_t *tempNode = *head;	
+	listint_t *tempNode = *head;
 
 	unsigned int count = 0;
 
