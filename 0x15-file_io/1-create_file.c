378c84db2a799c983bd42892526c6d3e55062f06
Cecilia Mulandi <mulandicecilia4@gmail.com>
2023-04-10 15:10:50 -0700

    changes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,9 +19,9 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-		while (text_content[i])
-			i++;
-		filewrite = write(newfile, text_content, i);
+		while (text_content[j])
+			j++;
+		filewrite = write(newfile, text_content, j);
 
 		if (filewrite == -1)
 		{
